9ca163dbf22960ca3e22c571c7b4a14a0c399df7
Daniel Rector <drector@vmware.com>
2022-06-04 17:55:59 -0400

    first commit

---
+++ functions.cpp
@@ -0,0 +1,22 @@
+#include "functions.h"
+
+double add(double x, double y)
+{
+    return x + y;
+}
+
+double add(double a, double b, double c)
+{
+    return a + b + c;
+    //alternatively: return add(add(a,b),c);
+}
+
+bool test(bool x)
+{
+    return x;
+}
+
+bool test(double x)
+{
+    return x > 0;
+}
